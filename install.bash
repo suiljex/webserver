b930d3ea6849121b02f6e6976161e63cc1360090
Igor G <account@suiljex.space>
2022-01-21 21:56:31 +0700

    fix install script

--- install.bash
+++ install.bash
@@ -9,20 +9,21 @@ PACKAGES_APT="nginx python3 python3-pip openssl"
 PACKAGES_PIP="certbot"
 
 CMD_INSTALL_APT="apt-get update && apt-get install -y "
-CMD_INSTALL_PIP="pip3 install "
+CMD_INSTALL_PIP="pip3 install --upgrade --upgrade-strategy=only-if-needed "
 CMD_MKDIR="mkdir -p "
-CMD_MV="mv "
-CMD_SETUID="chmod u+s "
+CMD_CP="cp "
 
 #eval ${CMD_UPDATE}
-eval ${CMD_INSTALL_PKG} ${PACKPACKAGES_APTAGES}
+eval ${CMD_INSTALL_APT} ${PACKAGES_APT}
 eval ${CMD_INSTALL_PIP} ${PACKAGES_PIP}
 
 eval ${CMD_MKDIR} "/var/www/letsencrypt"
 eval ${CMD_MKDIR} "/etc/letsencrypt"
 eval ${CMD_MKDIR} "/etc/nginx/conf.d"
 
-eval ${CMD_MV} "${SCRIPT_FULL_PATH}/certctl"        "/usr/bin/certctl"
-eval ${CMD_MV} "${SCRIPT_FULL_PATH}/nginx_conf.d/*" "/etc/nginx/conf.d/"
+eval ${CMD_CP} "${SCRIPT_FULL_PATH}/certctl"        "/usr/bin/certctl"
+eval ${CMD_CP} "${SCRIPT_FULL_PATH}/nginx_conf.d/*" "/etc/nginx/conf.d/"
 
-eval ${CMD_SETUID} "/usr/bin/certctl"
+# chown root "/usr/bin/certctl"
+# chmod u+s  "/usr/bin/certctl"
+chmod +x   "/usr/bin/certctl"
