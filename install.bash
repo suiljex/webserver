1db8ff14b5bf963b82ecb209de69d2bd35ad4726
Igor G <account@suiljex.space>
2022-01-21 20:24:19 +0700

    fix install

--- install.bash
+++ install.bash
@@ -5,16 +5,18 @@
 SCRIPT_NAME=$0
 SCRIPT_FULL_PATH=$(dirname "$0")
 
-PACKAGES="nginx certbot"
+PACKAGES_APT="nginx python3 python3-pip openssl"
+PACKAGES_PIP="certbot"
 
-CMD_INSTALL_PKG="apt-get install -y "
-CMD_UPGRADE="apt-get update && apt-get upgrade -y"
+CMD_INSTALL_APT="apt-get update && apt-get install -y "
+CMD_INSTALL_PIP="pip3 install "
 CMD_MKDIR="mkdir -p "
 CMD_MV="mv "
 CMD_SETUID="chmod u+s "
 
-eval ${CMD_UPGRADE}
-eval ${CMD_INSTALL_PKG} ${PACKAGES}
+#eval ${CMD_UPDATE}
+eval ${CMD_INSTALL_PKG} ${PACKPACKAGES_APTAGES}
+eval ${CMD_INSTALL_PIP} ${PACKAGES_PIP}
 
 eval ${CMD_MKDIR} "/var/www/letsencrypt"
 eval ${CMD_MKDIR} "/etc/letsencrypt"
